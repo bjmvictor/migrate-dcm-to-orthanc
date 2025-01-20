238d16f112e9b58bf56ffdf7462a671dcf92a373
Benjamin Victor <bjm.victor@gmail.com>
2025-01-20 09:43:18 -0300

    Added multiples folders import and config file

---
+++ config.py
@@ -0,0 +1,16 @@
+# Orthanc access
+useHttps = False  # False(http) or True(https)
+host = 'localhost'
+port = '8042'
+user = ''
+password = ''
+
+# (INT) Max of sending tasks (Recommended 10-30)
+maxTasks = 20 
+
+# Retries after failure
+maxRetries = 5 # Times
+retryInterval = 5 # Seconds
+
+# Logs directory folder
+logDir = 'logs'
--- migrate.py
+++ migrate.py
@@ -2,12 +2,13 @@ import os
 import asyncio
 import aiohttp
 import aiofiles
+import config
 from aiohttp.helpers import BasicAuth
 import logging
 from tkinter import Tk, filedialog
 
-# Configuração dos arquivos de log
-LOG_DIR = "logs"
+# Logfile configuration
+LOG_DIR = config.logDir
 os.makedirs(LOG_DIR, exist_ok=True)
 LOG_FILE = os.path.join(LOG_DIR, "upload_log.txt")
 ERROR_LOG_FILE = os.path.join(LOG_DIR, "error_log.txt")
@@ -25,39 +26,48 @@ error_logger = logging.getLogger("error_logger")
 error_logger.addHandler(logging.FileHandler(ERROR_LOG_FILE))
 
 async def main():
-    # Seleção de pastas pelo usuário
+    # Select dicom folder
     root_dirs = select_folders()
     if not root_dirs:
-        logging.info("[INFO] Nenhuma pasta selecionada. Encerrando o programa.")
+        logging.info("[INFO] No folder was selected, closing.")
         return
-
-    upload_url = r'http://192.168.1.200:8042/instances'
-    max_concurrent_posts = 10  # Máximo de tarefas simultâneas
+    
+    prefix = 'https' if config.useHttps else 'http'
+    
+    upload_url = f'{prefix}://{config.host}:{config.port}/instances'
+    max_concurrent_posts = config.maxTasks  # Max of tasks
     semaphore = asyncio.Semaphore(max_concurrent_posts)
     
     for root_dir in root_dirs:
-        logging.info(f"[INFO] Procurando diretórios na pasta raiz: {root_dir}")
+        logging.info(f"[INFO] Finding for directories on root: {root_dir}")
         subdirectories = find_subdirectories(root_dir)
 
         for subdirectory in subdirectories:
             dicom_files = find_dicom_files(subdirectory)
             if dicom_files:
-                logging.info(f"[INFO] Encontrado {len(dicom_files)} arquivos DICOM em {subdirectory}")
+                logging.info(f"[INFO] Find {len(dicom_files)} dicom files on {subdirectory}")
                 await process_dicom_files(dicom_files, upload_url, semaphore)
             else:
-                logging.info(f"[INFO] Nenhum arquivo DICOM encontrado em {subdirectory}")
+                logging.info(f"[INFO] No dicom files found on {subdirectory}")
     
-    logging.info("[INFO] Todas as pastas processadas.")
+    logging.info("[INFO] All folders have been completed.")
 
 def select_folders():
-    """Permite ao usuário selecionar uma ou mais pastas."""
-    Tk().withdraw()  # Oculta a janela principal do Tkinter
-    logging.info("[INFO] Selecione as pastas para processar:")
-    root_dirs = filedialog.askdirectory(mustexist=True, title="Selecione a(s) pasta(s) contendo os arquivos DICOM", multiple=True)
-    return root_dirs
+    """Allows the user to select one or more folders."""
+    Tk().withdraw()  # Hide the TKinter window
+    logging.info("[INFO] Select one or more folders. Click Cancel when finished.")
+    directories = []
+    while True:
+        folder = filedialog.askdirectory(mustexist=True, title="Select a folder containing the DICOM files")
+        if not folder:  # If the user clicks "Cancel"
+            break
+        directories.append(folder)
+        logging.info(f"[INFO] Selected: {folder}")
+    return directories
+
 
 def find_subdirectories(root_dir):
-    """Procura todos os subdiretórios da pasta raiz."""
+    """Find all subdirectories on root's folder"""
     subdirectories = []
     for dirpath, dirnames, _ in os.walk(root_dir):
         for dirname in dirnames:
@@ -66,7 +76,7 @@ def find_subdirectories(root_dir):
     return subdirectories
 
 def find_dicom_files(directory):
-    """Procura todos os arquivos DICOM no diretório recebido."""
+    """Searches all DICOM files in the received directory"""
     dicom_files = []
     for dirpath, _, filenames in os.walk(directory):
         for filename in filenames:
@@ -76,11 +86,11 @@ def find_dicom_files(directory):
     return dicom_files
 
 def is_dicom_file(filename):
-    """Determina se um arquivo é do tipo DICOM baseado na extensão do arquivo."""
+    """Determines whether a file is of type DICOM based on the file extension"""
     return filename.lower().endswith('.dcm') or '.' not in filename
 
 async def process_dicom_files(dicom_files, upload_url, semaphore):
-    """Envia todos os arquivos DICOM da lista para o servidor Orthanc."""
+    """Sends all DICOM files in the list to the Orthanc server"""
     async with aiohttp.ClientSession() as session:
         tasks = [
             asyncio.create_task(upload_file(dicom_file, upload_url, semaphore, session))
@@ -89,35 +99,35 @@ async def process_dicom_files(dicom_files, upload_url, semaphore):
         await asyncio.gather(*tasks)
 
 async def upload_file(file_path, upload_url, semaphore, session):
-    """Envia um arquivo para o servidor Orthanc com autenticação básica."""
+    """Sends a file to the Orthanc server with basic authentication."""
     async with semaphore:
-        max_retries = 5
-        retry_interval = 5
+        max_retries = config.maxRetries
+        retry_interval = config.retryInterval
 
         for attempt in range(1, max_retries + 1):
             try:
                 async with aiofiles.open(file_path, 'rb') as f:
                     data = await f.read()
 
-                auth = BasicAuth('admin', 'Fmsa@3459')
+                auth = BasicAuth(config.user, config.password)
                 async with session.post(upload_url, data=data, auth=auth) as resp:
                     if resp.status in {200, 201}:
                         if attempt > 1:
                             error_logger.error(
-                                f"[ERROR] Arquivo {file_path} falhou inicialmente, mas foi enviado após {attempt} tentativa(s)."
+                                f"[ERROR] File {file_path} has send after {attempt} trie(s)."
                             )
-                        logging.info(f"[INFO] Enviado {file_path}")
+                        logging.info(f"[INFO] Send {file_path}")
                         return
                     else:
-                        logging.warning(f"[ERROR] Falha no envio de {file_path}, código do erro: {resp.status}")
+                        logging.warning(f"[ERROR] Failed to send {file_path}, error code: {resp.status}")
             except Exception as e:
-                logging.error(f"[ERROR] Falha no upload de {file_path}: {e}")
+                logging.error(f"[ERROR] Fail to upload {file_path}: {e}")
 
             if attempt < max_retries:
-                logging.warning(f"[ERROR] Tentativa {attempt} falhou para {file_path}, tentando novamente em {retry_interval} segundos...")
+                logging.warning(f"[ERROR] Tries {attempt} failed to {file_path}, trying again in {retry_interval} seconds...")
                 await asyncio.sleep(retry_interval)
             else:
-                error_logger.error(f"[ERROR] Todas as tentativas falharam para o arquivo {file_path}.")
+                error_logger.error(f"[ERROR] All attempts failed for the file {file_path}.")
 
 if __name__ == '__main__':
     asyncio.run(main())
